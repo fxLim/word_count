4528d786be37a232a97fb979fa3b8cc38a26e2e4
fxLim <53017452+fxLim@users.noreply.github.com>
2021-01-27 14:45:12 +0100

    Add files via upload

---
+++ word_count/cmake-build-debug/CMakeFiles/word_count.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/word_count.dir/main.cpp.obj"
+  "libword_count.dll.a"
+  "word_count.exe"
+  "word_count.exe.manifest"
+  "word_count.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/word_count.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
