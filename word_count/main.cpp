2ef6b481c253f9d448b6801218b82d979bd44994
fxLim <53017452+fxLim@users.noreply.github.com>
2021-01-28 15:02:58 +0100

    Added class

--- word_count/main.cpp
+++ word_count/main.cpp
@@ -6,113 +6,137 @@
 #include <sys/stat.h>
 #include <windows.h>
 #include <stdio.h>
-#include <vector>
-#include <set>
 
 using namespace std;
 
 BOOL WINAPI consoleHandler(DWORD signal) {
 
-    if (signal == CTRL_C_EVENT) {
-        printf("\nVom Benutzer beendet.\n"); // do cleanup
-        exit(0);
+    if (CTRL_C_EVENT == signal) {
+    printf("\nVom Benutzer beendet.\n");
+    exit(0);
     }
 
     return TRUE;
 }
 
-long GetFileSize(std::string filename) {
-    struct stat stat_buf;
-    int rc = stat(filename.c_str(), &stat_buf);
-    return rc == 0 ? stat_buf.st_size : -1;
-}
-
-void printProgress(float progress) {
-    if (progress <= 1.0) {
-        int barWidth = 70;
-        cout << "\r[";
-        int pos = barWidth * progress;
-        for (int i = 0; i < barWidth; ++i) {
-            if (i < pos) cout << "=";
-            else if (i == pos) cout << ">";
-            else cout << " ";
-        }
-        cout << "] " << int(progress * 100.0) << "% \r";
-        cout.flush();
-    }
-}
-
-multimap<int, string> invert(map<string, int> &mymap) {
-    multimap<int, string> multiMap;
-
-    map<string, int>::iterator it;
-    for (it = mymap.begin(); it != mymap.end(); it++) {
-        multiMap.insert(make_pair(it->second, it->first));
-    }
-
-    return multiMap;
-}
-
-int main() {
-    //running = TRUE;
-    if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
-        printf("\nERROR: Could not set control handler");
-        return 1;
-    }
-    vector<pair<string, int>> vec;
-    static char filename[] = "";
-    cout << "Datei speicherort eingeben: ";
-    cin >> filename;
-    long fileSize = GetFileSize(filename);
-    float progress = 0;
-    int progressLast = 0;
-    //cout << fileSize << endl;
-
-    long byteCount = 0;
-    ifstream input(filename);
-    if (!input) {
-        cout << "Fehler beim Öffnen der Datendatei " << filename << "\n";
-        return 1;
-    }
-    cout << "Ven zie den Vorgang stoppen möchten drücken Sie CTRL + C!" << endl;
-    cout << "Die datai \"" << filename << "\" wird verarbeitet:" << endl;
-    map<string, int> word_data;
-    string word;
-    // d:\_seltron\my_data1.txt
-    while (input >> word) {
-        byteCount = byteCount + word.length() + 1;
-        progress = ((float) byteCount / fileSize);
-        if (progressLast != (int) (progress * 100)) {
-            printProgress(progress);
-            progressLast = progress * 100;
+class wordCounter {
+private:
+    void parseDoc(const string filename, map<string, int> &wordData, long fileSize) {
+        ifstream input(filename);
+        if (!input)
+        {
+            cout << "Fehler beim Öffnen der Datei " << filename << "\n";
+            exit(1);
         }
 
-        if (word_data.find(word) != word_data.end()) {
-            word_data[word]++;
-        } else {
-            word_data[word] = 1;
+        float progress;
+        int progressLast = 0;
+        long byteCount = 0;
+
+        cout << "Ven zie den Vorgang stoppen möchten drücken Sie CTRL + C!" << endl;
+        cout << "Die datai \"" << filename << "\" wird verarbeitet:" << endl;
+
+//        map<string, int> wordData;
+        string word;
+        while (input >> word) {
+            byteCount = byteCount + word.length() + 1;
+            progress = ((float)byteCount / fileSize);
+            if(progressLast != (int)(progress*100)) {
+                printProgress(progress);
+                progressLast = progress*100;
+            }
+            if (wordData.find(word) != wordData.end()) {
+                wordData[word]++;
+            }
+            else {
+                wordData[word] = 1;
+            }
+        }
+        printProgress(1);
+    }
+
+    void printProgress(float progress) {
+        if (1 >= progress) {
+            const int barWidth = 70;
+            cout << "\r[";
+            int pos = barWidth * progress;
+            for (int i = 0; i < barWidth; ++i) {
+                if (i < pos) cout << "=";
+                else if (i == pos) cout << ">";
+                else cout << " ";
+            }
+            cout << "] " << int(progress * 100.0) << "% \r";
+            cout.flush();
         }
     }
-    printProgress(1);
-    cout << "\n";
-    cout.width(20);
-    cout << left << "Wort";
-    cout << left << "Anzahl\n";
 
-    multimap<int, string> newmap = invert(word_data);
-    multimap<int, string>::iterator iter;
-    for (iter = newmap.begin(); iter != newmap.end(); iter++) {
-        // printing the second value first because the
-        // order of (key,value) is reversed in the multimap
+    void printTable(map<string, int> wordData) {
+        cout << "\n";
         cout.width(20);
-        cout << left << iter->second;
-        cout << left << iter->first << endl;
+        cout << left << "Wort";
+        cout << left << "Anzahl\n";
+
+        multimap<int, string> newmap = invertMap(wordData);
+
+        for (auto const& [key, val] : newmap) {
+            cout.width(20);
+            cout << left << val;
+            cout << left << key << endl;
+        }
     }
-//    map<string, unsigned int>::iterator iter;
-//    for (iter = word_data.begin(); iter != word_data.end(); ++iter)
-//    {
-//        cout << iter->first << "\t\t\t" << iter->second << "\n";
-//    }
-    cin >> filename;
+
+    long getFileSize(string filename) {
+        struct stat statBuf;
+        int rc = !stat(filename.c_str(), &statBuf) ? statBuf.st_size : -1;
+        if (1 > rc ) {
+            cout << "ERROR: Datei existiert nicht oder ungültige Dateigröße.";
+            exit(1);
+        }
+        return statBuf.st_size;
+    }
+
+    //multimap elements can have the same keys
+
+    multimap<int, string> invertMap(map<string, int> &map) {
+        multimap<int, string> multiMap;
+
+        for (auto const& [key, val] : map) {
+            multiMap.insert(make_pair(val, key));
+        }
+
+        return multiMap;
+    }
+
+public:
+    map<string, int> wordData;
+    long fileSize;
+    wordCounter(const string filename) {
+        fileSize = getFileSize(filename);
+        parseDoc(filename, wordData, fileSize);
+    }
+    void printWordData() { printTable(wordData);}
+};
+
+int main(int argc, char *argv[]) {
+    if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
+       cout << "\nERROR: Control Handler konnte nicht eingestellt werden." << endl;
+        
+        return 1;
+    }
+    if (1 == argc) {
+        cout << "ERROR: Bitte datei speicherort als Argument eingeben." << endl;
+
+        return 1;
+    }
+    else if (2 < argc) {
+        cout << "ERROR: Ungültige Anzahl von Argumenten." << endl;
+
+        return 1;
+    }
+    
+    const string filename = argv[1];
+    wordCounter doc(filename);
+    doc.printWordData();
+    cout << doc.fileSize;
     return 0;
 }
